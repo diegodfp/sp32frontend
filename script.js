464c0ff9efe4ed8d36872431c808587d679cada4
diegodfp <diegodfp.fly@gmail.com>
2024-09-30 02:07:08 -0500

    ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const apiUrl = 'https://apisp32.onrender.com/api/sensor-data';
+const apiUrl = 'https://apisp32-production.up.railway.app/api/sensor-data';
 
 async function fetchData() {
   try {

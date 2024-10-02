b4dcb5ad70f661ae7e1d0eb4c6698ac9464f73bd
diegodfp <diegodfp.fly@gmail.com>
2024-10-02 12:25:40 -0500

    incluido promedio

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 const apiUrl = 'https://apisp32.onrender.com/api/data-sensor/list';
+const averagesUrl = 'https://apisp32.onrender.com/api/data-sensor/averages';
 
 async function fetchData() {
   try {
-    const response = await fetch( apiUrl);
+    const response = await fetch(apiUrl);
     
     if (!response.ok) {
       throw new Error('Error al obtener los datos: ' + response.statusText);
@@ -20,9 +20,35 @@ async function fetchData() {
         <p>Fecha y hora: ${new Date(item.timestamp).toLocaleString()}</p>
       </div>
     `).join('');
+
+    fetchAverages();
+
   } catch (error) {
     console.error('Error:', error);
     document.getElementById('data-container').textContent = 'Error al cargar los datos.';
+  }
+}
+
+async function fetchAverages() {
+  try {
+    const response = await fetch(averagesUrl);
+    
+    if (!response.ok) {
+      throw new Error('Error al obtener los promedios: ' + response.statusText);
+    }
+
+    const averages = await response.json();
+    
+    const dataContainer = document.getElementById('data-container');
+    dataContainer.innerHTML += `
+      <div>
+        <h2>Promedios de Hoy</h2>
+        <p>Temperatura Promedio: ${averages.averageTemperature}°C</p>
+        <p>Humedad Promedio: ${averages.averageHumidity}%</p>
+      </div>
+    `;
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
 
